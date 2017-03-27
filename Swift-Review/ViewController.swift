173fd6cc2b4ff7ca2e7d756a1f95aec1d1de90bb
Nguyen Van TRUNG <trung.nv@altplus.com.vn>
2017-03-27 16:26:32 +0700

    Add New

---
+++ Swift-Review/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Review
+//
+//  Created by Nguyen Van TRUNG on 3/27/17.
+//  Copyright © 2017 altplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
